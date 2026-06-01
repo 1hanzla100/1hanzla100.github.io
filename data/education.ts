425b8e3c769be0c743b24afc74c6574fccc46fe4
1hanzla100 <hanzla.tauqeer123@gmail.com>
2026-06-01 17:41:50 +0500

    Removed Duration of Edu

--- data/education.ts
+++ data/education.ts
@@ -2,8 +2,8 @@ export const education = [
 	{
 		schoolName: "University of Management and Technology",
 		subHeader: "Bachelor of Science in Computer Science",
-		duration: "September 2023 - Present",
-		grade: "3.8/4.0 CGPA",
+		duration: "",
+		grade: "",
 		desc: "",
 		descBullets: [],
 	},
